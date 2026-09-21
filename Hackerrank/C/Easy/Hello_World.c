d0854e6e5114413b49b546976f5063afdb10f83b
Chaitali Shirsath <chaitali3611@gmail.com>
2026-09-21 23:21:37 +0530

    Solving DSA Problem

---
+++ Hackerrank/C/Easy/Hello_World.c
@@ -0,0 +1,8 @@
+int main() 
+{
+	printf("Hello, World!");
+    printf("\nWelcome to C programming.");
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
