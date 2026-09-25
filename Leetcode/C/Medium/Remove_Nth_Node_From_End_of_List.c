4b82089388a9b8a44087c2558dd860d81f393669
Chaitali Shirsath <chaitali3611@gmail.com>
2026-09-25 15:14:36 +0530

    Solving Medium level DSA Problem

---
+++ Leetcode/C/Medium/Remove_Nth_Node_From_End_of_List.c
@@ -0,0 +1,23 @@
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    struct ListNode dummy;
+    dummy.next = head;
+
+    struct ListNode* fast = &dummy;
+    struct ListNode* slow = &dummy;
+
+    for (int i = 0; i < n; i++) {
+        fast = fast->next;
+    }
+
+    while (fast->next != NULL) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    struct ListNode* temp = slow->next;
+    slow->next = slow->next->next;
+
+    free(temp);
+
+    return dummy.next;
+}
